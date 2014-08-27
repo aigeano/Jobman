4041e2cda96bb433f5aefb3af7e9d67e46f94f75
aigeano <aigeano@gmail.com>
2014-08-27 07:07:46 +0530

    all jobs and filter by location working

--- app.py
+++ app.py
@@ -6,70 +6,80 @@ app = Flask(__name__)
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
-        url = "https://api.angel.co/1/jobs.json"
-        data = json.loads(requests.get(url).text)
+        for index in range(1,255):
+            res= []
+            data = json.load(open("filtered.json"))
+            fil = ['developer','technical','engineer','testing']
 
-	errors = ''
-	if request.method == "GET":
-		return render_template("index.html", errors=errors)
-	else:
+	    errors = ''
+	    if request.method == "GET":
+		    return render_template("index.html", errors=errors)
+	    else:
 		
-                key  = request.form['keyword']
-		loc = request.form['location']
+                    key = request.form['keyword']
+		    loc = request.form['location']
 
-        if key and not loc :
+            if key and not loc :
                 
-                k = key.lower()
-                count = 0 
-                res= []
-                for n in range(50):
-                        if k in data["jobs"][n]['title'].lower() or k in data["jobs"][n]['description'].lower():
-                                res.append( "job id: " + str(data["jobs"][n]['id']) + " and Job Title-> " + data["jobs"][n]['title'])
-                                count = count + 1
-                if count==0:
-                        res.append( "No matches found for "+ k)
+                    k = key.lower()
+                    count = 0 
+                    res= []
+                    for n in range(5750):
+                            if k in data[n]['title'].lower() or k in data[n]['description'].lower():
+                                    t =data[n]['title'].lower()
+                                    if fil[0] not in t and fil[1] not in t and fil[2] not in t and fil[3] not in t :
+                                            res.append( "job id: " + str(data[n]['id']) + " Job Title-> " + data[n]['title'])
+                                            count = count + 1
+                    if count==0:
+                            res.append( "No matches found for "+ k)
                         
-        elif loc and not key :
-                l = loc.lower()
-                count = 0
-                res = []
-                for n in range(50):
-                        for i in range(len(data["jobs"][n]['tags'])):
-                                if 'LocationTag' in data["jobs"][n]['tags'][i]['tag_type']:
-                                        if l in data["jobs"][n]['tags'][i]['name']:
-                                                res.append( "job id: " + str(data["jobs"][n]['id']) + " and Job Title-> " + data["jobs"][n]['title'])
-                                                count = count +1
-                if count == 0 :
-                        res.append( "No matches found for location "+ l)
+            elif loc and not key :
+                    l = loc.lower()
+                    count = 0
+                    res = []
+                    for n in range(5750):
+                            t = data[n]['title'].lower()
+                            for i in range(len(data[n]['tags'])):
+                                    if 'LocationTag' in data[n]['tags'][i]['tag_type']:
+                                            if l in data[n]['tags'][i]['name']:
+                                                    if fil[0] not in t and fil[1] not in t and fil[2] not in t and fil[3] not in t :
+                                                            res.append( "job id: " + str(data[n]['id']) + " and Job Title-> " + data[n]['title'])
+                                                            count = count +1
+                    if count == 0 :
+                            res.append( "No matches found for location "+ l)
                   
-        elif loc and key :
-	        a = key.lower()
-                b = loc.lower()
-                count = 0 
-                res = []
-                for n in range(50):
-                        for i in range(len(data["jobs"][n]['tags'])):
-                                if 'LocationTag' in data["jobs"][n]['tags'][i]['tag_type']:
-                                        if b in data["jobs"][n]['tags'][i]['name']:
-                                                if a in data["jobs"][n]['title'].lower() or a in data["jobs"][n]['description'].lower():
-                                                        res.append( "job id: " + str(data["jobs"][n]['id']) + " and Job Title-> " + data["jobs"][n]['title'])
-                                                        count = count + 1
-                if count == 0 :
-                       res.append( "No matches Found. Try another combination")
+            elif loc and key :
+	            a = key.lower()
+                    b = loc.lower()
+                    count = 0 
+                    res = []
+                    for n in range(5750):
+                            t = data[n]['title'].lower()
+                            for i in range(len(data[n]['tags'])):
+                                    if 'LocationTag' in data[n]['tags'][i]['tag_type']:
+                                            if b in data[n]['tags'][i]['name']:
+                                                    if a in data[n]['title'].lower() or a in data[n]['description'].lower():
+                                                            if fil[0] not in t and fil[1] not in t and fil[2] not in t and fil[3] not in t :
+                                                                    res.append( "job id: " + str(data[n]['id']) + " and Job Title-> " + data[n]['title'])
+                                                                    count = count + 1
+                    if count == 0 :
+                           res.append( "No matches Found. Try another combination")
             
             
-        else :
-               res = []
-               for n in range(50):
-                       res.append( "job id: " + str(data["jobs"][n]['id']) + "and Job Title-> " + data["jobs"][n]['title'])
-                   
+            else :
+                   res = []
+                   for n in range(5750):
+                           t = data[n]['title'].lower()
+                           if fil[0] not in t and fil[1] not in t and fil[2] not in t and fil[3] not in t :
+                                   res.append( "job id: " + str(data[n]['id']) + "and Job Title-> " + data[n]['title'])
+                    
              		
-        data = {
+            data = {
 	        'result': res,
 				
 			}			
-        return render_template('data.html', data=data)
-        return render_template('index.html', errors=errors)
+            return render_template('data.html', data=data)
+            return render_template('index.html', errors=errors)
 
 
 if __name__ == '__main__':
